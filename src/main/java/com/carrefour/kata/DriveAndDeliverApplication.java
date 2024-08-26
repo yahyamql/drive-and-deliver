9624c70f9bb0600f4b6af6660a8c9f35a5219403
yahyamql <shiniga.yox@gmail.com>
2024-08-26 15:15:40 +0100

    kata-1 init project

---
+++ src/main/java/com/carrefour/kata/DriveAndDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.carrefour.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveAndDeliverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveAndDeliverApplication.class, args);
+    }
+
+}
